c5bee32d791216e6548d49a58cb37c24f0cc771e
ndeztea <imac@iMacs-iMac.local>
2015-09-21 20:02:24 +0700

    debug mode on

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'uzIyp2I0j4IiGqLIouu9kxN1q1TrSNCy'),
 
     'cipher' => 'AES-256-CBC',
 
