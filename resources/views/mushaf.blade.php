1ccc9d728b9760ff5ac05e91b1ae6ddfd7714c46
Ara S <asep.koswara@gmail.com>
2016-02-12 00:39:42 +0700

    rearrange things

--- resources/views/mushaf.blade.php
+++ resources/views/mushaf.blade.php
@@ -9,36 +9,47 @@ $prev_surah = '';
 @section('content')
 	@include('players')
 
-	<div class="nav top">
-		<select name="surah" onchange="QuranJS.changeSurah(this)" >
-			<?php foreach($surahs as $surah):?>
-			<option  <?php echo $surah->id==$ayats[0]->surah?'selected':''?> value="<?php echo $surah->id ?>"><?php echo $surah->id ?>. <?php echo $surah->surah_name ?></option>
-			<?php endforeach?>
-		</select>
-
-		<div class="btn-group" role="group" aria-label="Navigasi">
-			<a class="btn btn-default" href="javascript:;" onclick="QuranJS.changePage(this)" data-value="<?php echo $curr_page-1?>">
-				<span class="hidden-xs"><?php echo trans('trans.prev')?></span>
-				<span class="visible-xs"><i class="fa fa-angle-left"></i></span>
-			</a>
-			<a class="btn btn-default" href="javascript:;" onclick="QuranJS.changePage(this)" data-value="<?php echo $curr_page+1?>">
-				<span class="hidden-xs"><?php echo trans('trans.next')?></span>
-				<span class="visible-xs"><i class="fa fa-angle-right"></i></span>
-			</a>
+	<div class="nav-top clearfix">
+		
+		<div class="pull-left select-surah">
+			<select class="selectpicker form-control" name="surah" onchange="QuranJS.changeSurah(this)" >
+				<?php foreach($surahs as $surah):?>
+				<option  <?php echo $surah->id==$ayats[0]->surah?'selected':''?> value="<?php echo $surah->id ?>"><?php echo $surah->id ?>. <?php echo $surah->surah_name ?></option>
+				<?php endforeach?>
+			</select>
 		</div>
+		<!-- /select-surah -->
 
-		
-		<select name="page" onchange="QuranJS.changePage(this)" >
-			<?php foreach($pages as $page):?>
-			<option  <?php echo $page->page==$curr_page?'selected':''?> value="<?php echo $page->page ?>"><?php echo $page->page ?></option>
-			<?php endforeach?>
-		</select>
-		
+		<div class="surah-nav">
+			<div class="input-group" role="group" aria-label="Navigasi">
+				<a class="btn btn-default input-group-addon" href="javascript:;" onclick="QuranJS.changePage(this)" data-value="<?php echo $curr_page-1?>">
+					<span class="hidden-xs"><?php echo trans('trans.prev')?></span>
+					<span class="visible-xs"><i class="fa fa-angle-left"></i></span>
+				</a>
+				<select class="selectpicker form-control" name="page" onchange="QuranJS.changePage(this)" >
+					<?php foreach($pages as $page):?>
+					<option  <?php echo $page->page==$curr_page?'selected':''?> value="<?php echo $page->page ?>"><?php echo $page->page ?></option>
+					<?php endforeach?>
+				</select>
+				<a class="btn btn-default input-group-addon" href="javascript:;" onclick="QuranJS.changePage(this)" data-value="<?php echo $curr_page+1?>">
+					<span class="hidden-xs"><?php echo trans('trans.next')?></span>
+					<span class="visible-xs"><i class="fa fa-angle-right"></i></span>
+				</a>
+			</div>
+		</div>
+		<!-- /surah-nav -->
 
-		<span><input type="checkbox" id="automated_play" name="automated_play" checked ><?php echo trans('trans.play_otomatis')?></span>
+		<div class="surah-action pull-right">
+			<span class="auto-play">
+				<input type="checkbox" id="automated_play" name="automated_play" checked >&nbsp;<i class="fa fa-play-circle-o"></i>  <?php echo trans('trans.play_otomatis')?>
+			</span>
+			<a id="playNow" class="playnow"><i class="fa fa-play"></i> Play</a>
+		</div>
+		<!-- /surah-action -->
+
 	</div>
 	<!-- /nav-top -->
-	<a id="playNow">play</a>
+	
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12">
