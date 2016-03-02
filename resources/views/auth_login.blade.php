5239db281b211a8ad626087eb629276a7a68ace0
asepkoswara <asep.koswara@gmail.com>
2016-03-02 21:49:56 +0700

    Add small patch to register/login modal

--- resources/views/auth_login.blade.php
+++ resources/views/auth_login.blade.php
@@ -1,19 +1,13 @@
 <div class="login_form">
-	<form action="" onsubmit="return false" method="post">
+	<form class="form-horizontal" action="" onsubmit="return false" method="post">
 		<div class="form-group">
-			<div class="input-group">
-			  <span class="input-group-addon">Email</span>
-			  <input type="text" class="form-control" name="email" id="email" aria-label="email" />
-			</div>
+			<label for="email" class="control-label sr-only">Email</label>
+			<input type="text" class="form-control" name="email" id="email" aria-label="email" placeholder="Email" />
 		</div>
-		
 		<div class="form-group">
-			<div class="input-group">
-			  <span class="input-group-addon">Password</span>
-			  <input type="password" class="form-control" name="password" id="password" aria-label="password" />
-			</div>
+			<label for="password" class="control-label sr-only">Password</label>
+			<input type="password" class="form-control" name="password" id="password" aria-label="password" placeholder="Password" />
 		</div>
-		
 		<div class="form-group">
 			<div class="btn-group" role="group" aria-label="...">
 			  	<button type="button" class="btn btn-default btn-masuk" onclick="QuranJS.authProcess()">Masuk</button>
@@ -24,36 +18,26 @@
 </div>
 <!-- /login-form -->
 <div class="register_form" style="display:none">
-	<form action="" method="post" onsubmit="return false">
-
+	<form class="form-horizontal" action="" method="post" onsubmit="return false">
 		<div class="form-group">
-			<div class="input-group">
-			  <span class="input-group-addon">Full Name</span>
-			  <input type="name" class="form-control" name="name" id="name" aria-label="Full Name" />
-			</div>
+			<label for="name" class="control-label sr-only">Full Name</label>
+			<input type="name" class="form-control" name="name" id="name" aria-label="Full Name" placeholder="Full Name" />
 		</div>
-
 		<div class="form-group">
-			<div class="input-group">
-			  <span class="input-group-addon">Email</span>
-			  <input type="text" class="form-control" name="email" id="register_email" aria-label="email" />
-			</div>
-		</div>
+			<label for="email" class="control-label sr-only">Email</label>
+			<input type="text" class="form-control" name="email" id="register_email" aria-label="email" placeholder="Email" />
 
+		</div>
 		<div class="form-group">
-			<div class="input-group">
-			  <span class="input-group-addon">Password</span>
-			  <input type="password" class="form-control" name="password" id="register_password" aria-label="password" />
-			</div>
+			<label for="password" class="control-label sr-only">Password</label>
+			<input type="password" class="form-control" name="password" id="register_password" aria-label="password" placeholder="Password" />
 		</div>
-
 		<div class="form-group">
 			<div class="btn-group" role="group" aria-label="...">
 				<button type="button" class="btn btn-default btn-masuk" onclick="QuranJS.showLogin()"><i class="fa fa-angle-left"></i> Login Sekarang</button>
 				<button type="button" class="btn btn-default btn-daftar" onclick="QuranJS.registerProcess()">Daftar</button>
 			</div>
 		</div>
-		
 	</form>
 </div>
 <!-- /register-form -->
