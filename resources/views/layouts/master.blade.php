5eb1c0afc2c64ee4bce91d8745d285bf0704070d
Dimas <ndeztea@gmail.com>
2016-03-18 20:53:28 +0700

    #38 preloader (need to be styled)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -55,6 +55,12 @@
         <script type="text/javascript" src="<?php echo url('assets/jplayer/dist/add-on/jplayer.playlist.min.js')?>"></script>
     </head>
     <body class="@if($body_class) {{$body_class}} @endif">
+
+        <!-- pre loader-->
+        <div id="preloader">
+            <img src="<?php echo url('assets/images/loading.gif')?>">
+            <div class="loading">Mohon tunggu...</div>
+        </div>
         @section('sidebar')
            <!-- This is the master sidebar. -->
         @show
@@ -124,7 +130,14 @@
         </div>
       </div>
     </div>
+
     <script>
+        $(document).ready(function(){
+            $('#preloader').hide();
+        });
+        $(window).bind('beforeunload', function(){
+          $('#preloader').show();
+        });
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
