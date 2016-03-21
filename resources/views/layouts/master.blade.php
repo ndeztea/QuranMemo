9dee1db760eae48cfd0bdcd5428b734d19520f43
Dimas <ndeztea@gmail.com>
2016-03-21 17:09:03 +0700

    disable preloader

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -57,10 +57,10 @@
     <body class="@if($body_class) {{$body_class}} @endif">
 
         <!-- pre loader-->
-        <div id="preloader">
+        <!--div id="preloader">
             <img src="<?php echo url('assets/images/loading.gif')?>">
             <div class="loading">Mohon tunggu...</div>
-        </div>
+        </div-->
         @section('sidebar')
            <!-- This is the master sidebar. -->
         @show
@@ -132,12 +132,12 @@
     </div>
 
     <script>
-        $(document).ready(function(){
+        /*$(document).ready(function(){
             $('#preloader').hide();
         });
         $(window).bind('beforeunload', function(){
           $('#preloader').show();
-        });
+        });*/
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
