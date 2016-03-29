0ec4e1e9168ccfca361706dbbf22a3efd3105a80
Dimas <ndeztea@gmail.com>
2016-03-29 20:10:32 +0700

    blade refactoring

--- resources/views/mushaf_search.blade.php
+++ resources/views/mushaf_search.blade.php
@@ -17,7 +17,7 @@ $prev_surah = '';
 					<div class="page-title">
 						<h2 class="pull-left">Pencarian</h2>
 					</div>
-					<?php if(!empty($search_result)):?>
+					@if(!empty($search_result))
 					<div class="nav-top clearfix">
 						<div class="container-fluid">
 							<div class="collapse in" id="surah-collapse">
@@ -25,16 +25,13 @@ $prev_surah = '';
 									<div class="col-xs-12">
 										<div class="select-surah">
 
-											<form class="form-inline" method="post" action="<?php echo url('mushaf/search')?>">
+											<form class="form-inline" method="post" action="{{url('mushaf/search')}}">
 												<div class="form-group">
-													<select class="selectpicker form-control" name="surah">
+													<select class="selectpicker form-control" name="surah"  onchange="location.href='{{url('mushaf/searchKeyword?keyword='.$keyword.'&surah=')}}'+this.value">
 														<option value="0">- Di semua surah - </option>
-														<?php foreach($surahs as $surah):?>
-															<?php 
-																$selectedSurah = '';
-															?>
-														<option  <?php echo $selectedSurah?> value="<?php echo $surah->surah ?>"><?php echo $surah->surah_name ?></option>
-														<?php endforeach?>
+														@foreach($surahs as $surah)
+														<option  {{ $selected_surah==$surah->surah?'selected':'' }} value="{{$surah->surah}}">{{ $surah->surah_name }} ( {{ $surah->count }} ayat )</option>
+														@endforeach
 													</select>
 												</div>
 											</form>
@@ -45,7 +42,7 @@ $prev_surah = '';
 									</div>
 								</div>
 							</div>
-							Pencarian dengan kata <strong><?php echo $keyword?></strong> ada di <strong><?php echo $count_search?></strong> ayat
+							Pencarian dengan kata <strong>{{$keyword}}</strong> ada di <strong>{{$count_search}}</strong> ayat
 						</div>
 					</div>
 					<!-- /nav-top -->
@@ -55,39 +52,39 @@ $prev_surah = '';
 								<div class="col-xs-12">
 									<div class="mushaf">
 										<div class="search-result">
-										<?php foreach($search_result as $search):?>
+										@foreach($search_result as $search)
 											<div class="search-item">
-												<p><?php echo $search->text_indo?></p>
+												<p>{{$search->text_indo}}</p>
 												<div class="search-footer">
-													<a href="<?php echo url('mushaf/page/'.$search->page.'#surah_'.$search->surah.'_'.$search->ayat)?>">Surah <?php echo $search->surah_name?> : <?php echo $search->ayat?></a>
+													<a href="{{url('mushaf/page/'.$search->page.'#surah_'.$search->surah.'_'.$search->ayat)}}">Surah {{$search->surah_name}} : {{$search->ayat}}</a>
 												</div>
 												<hr>
 											</div>
-										<?php endforeach?>
+										@endforeach
 										</div>
-									<?php endif?>
+									@endif
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 					<!-- /mushaf -->
-					<?php if(isset($pages)):?>
+					@if($pages>0)
 							<div class="surah-nav">
 								<div class="input-group" role="group" aria-label="Navigasi">
 									<ul class="pagination">
-										<li><a href="#"> << <?php //echo trans('trans.prev')?></a></li>
-										<li><a href="#"> < <?php //echo trans('trans.prev')?></a></li>
-										<?php for($a=1;$a<=$pages;$a++):?>
-										<li  class="<?php echo $a==$page?'active':''?>"><a href="<?php echo url('mushaf/searchKeyword?keyword='.$keyword.'&page='.$a) ?>"><?php echo $a ?></a></li>
-										<?php endfor?>
-										<li><a href="#"> > <?php //echo trans('trans.next')?></a></li>
-										<li><a href="#"> >> <?php //echo trans('trans.next')?></a></li>
+										<li><a href="{{ url('mushaf/searchKeyword?keyword='.$keyword.'&page=1')}}{{!empty($selected_surah)?'&surah='.$selected_surah:''}}"> << </a></li>
+										<li><a href="{{ url('mushaf/searchKeyword?keyword='.$keyword.'&page=')}}{{$page>1?$page - 1:1}}{{!empty($selected_surah)?'&surah='.$selected_surah:''}}"> < </a></li>
+										@for($a=1;$a<=$pages;$a++)
+										<li  class="{{$a==$page?'active':''}}"><a href="{{ url('mushaf/searchKeyword?keyword='.$keyword.'&page='.$a)}}{{!empty($selected_surah)?'&surah='.$selected_surah:''}}">{{ $a }}</a></li>
+										@endfor
+										<li><a href="{{ url('mushaf/searchKeyword?keyword='.$keyword.'&page=')}}{{$page<$pages?$page + 1:$pages}}{{!empty($selected_surah)?'&surah='.$selected_surah:''}}"> > </a></li>
+										<li><a href="{{ url('mushaf/searchKeyword?keyword='.$keyword.'&page='.$pages)}}{{!empty($selected_surah)?'&surah='.$selected_surah:''}}"> >> </a></li>
 									</ul>
 								</div>
 							</div>
 							<!-- /surah-nav -->
-						<?php endif?>
+						@endif
 
 				</div>
 				<!-- /single-column -->
@@ -96,7 +93,7 @@ $prev_surah = '';
 		</div>
 		<!-- /main-content-wrap -->
 
-<script src="<?php echo url('assets/js/jquery.highlight.js')?>"></script>
+<script src="{{url('assets/js/jquery.highlight.js')}}"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
 		$('.mushaf').highlight('<?php echo $keyword?>');
