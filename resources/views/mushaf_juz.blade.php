c270632ec9724696a1bf145913043d75a3294631
asepkoswara <asep.koswara@gmail.com>
2016-05-11 23:25:29 +0700

    Styled modal juz

--- resources/views/mushaf_juz.blade.php
+++ resources/views/mushaf_juz.blade.php
@@ -1,4 +1,4 @@
-<ul>
+<ul class="juz-list clearfix">
 @foreach($juzs as $juz)
 	<li>
 		<a href="{{url('mushaf/page/'.$juz->page)}}"><strong>{{$juz->id}}</strong> <span class="juz-arabic">{{$juz->juz_arabic}}</span> <span class="juz-indo">{{$juz->juz_indo}}</span></a>
