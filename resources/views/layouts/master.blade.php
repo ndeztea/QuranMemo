e4f976dfa61da98f6e14f94bc028e513b526cba9
asepkoswara <asep.koswara@gmail.com>
2016-06-06 04:19:58 +0700

    fixed horizontal scroll

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -122,7 +122,7 @@
         </div>
 
         <nav class="navbar navbar-default qm-navbar">
-            <div class="container-fluid">
+            <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#qm-navbar-collapse" aria-expanded="false">
