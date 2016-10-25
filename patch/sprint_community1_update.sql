214c51c0a3771e7cd8d8727b918e6ab036eff42d
Dimas <ndeztea@gmail.com>
2016-10-25 17:51:04 +0700

    update DB patch

--- patch/sprint_community1_update.sql
+++ patch/sprint_community1_update.sql
@@ -44,3 +44,58 @@ ALTER TABLE `memo_target`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 
 ALTER TABLE `memo_target` ADD `record` VARCHAR(100) NULL AFTER `note`;
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 25, 2016 at 12:50 PM
+-- Server version: 5.5.42
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `db_qurannote`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `memo_target_correction`
+--
+
+CREATE TABLE `memo_target_correction` (
+  `id` int(11) NOT NULL,
+  `id_memo_target` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `correction` text NOT NULL,
+  `note` text,
+  `status` int(11) DEFAULT '0',
+  `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `memo_target_correction`
+--
+ALTER TABLE `memo_target_correction`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_memo_target` (`id_memo_target`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `memo_target_correction`
+--
+ALTER TABLE `memo_target_correction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
