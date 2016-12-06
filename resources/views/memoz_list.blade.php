cd4d701bada0ccd3b15c7384527a61c073cbbd80
asepkoswara <asep.koswara@gmail.com>
2016-12-06 21:42:43 +0700

    update memoz list

--- resources/views/memoz_list.blade.php
+++ resources/views/memoz_list.blade.php
@@ -12,10 +12,10 @@
 		<?php $ayat_target = $row->ayat_end==0?$row->ayat_start:$row->ayat_start.'-'.$row->ayat_end?>
 		<div class="memoz-item memoz-{{$row->id}}">
 			<div class="memoz-body" onclick="location.href='{{url('memoz/surah/'.$row->surah_start.'/'.$ayat_target.'/'.$row->id)}}'">
-				<strong><a href="{{url('memoz/surah/'.$row->surah_start.'/'.$ayat_target.'/'.$row->id)}}">{{$row->surah}} : {{$ayat_target}}</a></strong>
+				<a class="memoz-link-surah" href="{{url('memoz/surah/'.$row->surah_start.'/'.$ayat_target.'/'.$row->id)}}"><strong>{{$row->surah}} : {{$ayat_target}}</strong></a>
 				<span class="memoz-target"> 
 					<i>{{dayDiff($row->date_start,$row->date_end)->days}} hari</i>
-					&bull; <i>@if($row->status==0) Belum Hafal @else Hafan dan Menunggu koreksi @endif</i>
+					<span class="spacer1">&bull;</span> <i>@if($row->status==0) Belum Hafal @else Hafan dan Menunggu koreksi @endif</i>
 				</span>
 				<p>{{$row->note}}</p>
 			</div>
