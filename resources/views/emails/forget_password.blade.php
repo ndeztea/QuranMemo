309b80bccdb566489ca77bbbfebb8249a9835f04
Dimas <ndeztea@gmail.com>
2016-12-22 16:26:08 +0700

    add comma

--- resources/views/emails/forget_password.blade.php
+++ resources/views/emails/forget_password.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master_email')
 
 @section('content')
-<p>Assamualaikum wr wb <strong>{{$emailData['name']}}</strong> </p>
+<p>Assamualaikum wr wb <strong>{{$emailData['name']}},</strong> </p>
 <p>
 Password baru untuk login ke QuranMemo adalah : </p>
 <strong class="password">{{$emailData['newPassword']}}</strong>
