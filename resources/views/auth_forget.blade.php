25334c0877e90bab92645f78acf2c7699ee4e79f
Dimas <ndeztea@gmail.com>
2017-01-04 21:03:00 +0700

    auth and forget password action

--- resources/views/auth_forget.blade.php
+++ resources/views/auth_forget.blade.php
@@ -1,12 +1,12 @@
 <div class="login_form">
-	<form class="form-horizontal" action="" onsubmit="return false" method="post">
+	<form class="form-horizontal" action="" onsubmit="QuranJS.forgetProcess();return false" method="post">
 		<div class="form-group">
 			<label for="email" class="control-label sr-only">Email</label>
 			<input type="email" class="form-control" name="login_email" id="login_email" aria-label="email" placeholder="Email" />
 		</div>
 		<div class="form-group">
 			<div class="btn-group" role="group" aria-label="...">
-			  	<button type="button" class="btn btn-default btn-masuk" onclick="QuranJS.forgetProcess()">
+			  	<button type="submit" class="btn btn-default btn-masuk" onclick="QuranJS.forgetProcess()">
 			  	<span class="label-masuk">Kirim ke Email</span>
 			  	<i class="fa fa-spinner fa-spin fa-3x fa-fw label-loading" style="display:none"></i>
 			  	</button>
--- resources/views/auth_login.blade.php
+++ resources/views/auth_login.blade.php
@@ -1,5 +1,5 @@
 <div class="login_form">
-	<form class="form-horizontal" action="" onsubmit="return false" method="post">
+	<form class="form-horizontal" action="" onsubmit="QuranJS.authProcess();return false" method="post">
 		<div class="form-group">
 			<label for="email" class="control-label sr-only">Email</label>
 			<input type="email" class="form-control" name="login_email" id="login_email" aria-label="email" placeholder="Email" />
@@ -10,7 +10,7 @@
 		</div>
 		<div class="form-group">
 			<div class="btn-group" role="group" aria-label="...">
-			  	<button type="button" class="btn btn-default btn-masuk" onclick="QuranJS.authProcess()">
+			  	<button type="submit" class="btn btn-default btn-masuk" onclick="QuranJS.authProcess()">
 			  	<span class="label-masuk">Masuk</span>
 			  	<i class="fa fa-spinner fa-spin fa-3x fa-fw label-loading" style="display:none"></i>
 			  	</button>
