4291204a4d3783d97be9359768b33f9582098d7c
Dimas <ndeztea@gmail.com>
2017-05-15 11:03:02 +0700

    save memoz button re-appear

--- resources/views/memoz.blade.php
+++ resources/views/memoz.blade.php
@@ -52,11 +52,9 @@
 					<div class="btn-group" role="group">
 						<a class="btn btn-green-small" href="javascript:;" onclick="QuranJS.createMemoModal()"><i class="fa fa-plus"></i></a>
 						<a class="btn btn-green-small" onclick="QuranJS.memozList()" href="javascript:void(0)"><i class="fa fa-file-text"></i></a>
+						<a class="btn btn-green-small" href="javascript:;"  btn-form-memoz" onclick="QuranJS.formMemoModal('{{$memoDetail->id}}')"><i class="fa fa-floppy-o"></i></a>
 						
-						
-					<!--a class="btn btn-green-small" href="{{url('memoz')}}"><i class="fa fa fa-thumbs-up"></i> Hafal</a-->
 						@if(!empty($memoDetail->id))
-						<button type="button" class="btn btn-green-small btn-form-memoz" onclick="QuranJS.formMemoModal('{{$memoDetail->id}}')"><i class="fa fa-floppy-o"></i></button>
 						<a class="btn btn-green-small" onclick="QuranJS.callModal('memoz/correction/list/{{$memoDetail->id}}')" href="javascript:void(0)"><i class="fa fa-check-square-o"></i></a>
 						@endif
 					</div>
