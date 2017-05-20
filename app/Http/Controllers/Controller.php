c9e3706c46a11b6ff7c21b5ad7383ebbe51abb97
Dimas <ndeztea@gmail.com>
2017-05-20 09:32:07 +0700

    fix autologin

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Users;
+
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
