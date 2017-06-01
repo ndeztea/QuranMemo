e3683d093279e48d0b84677e39d5fb52786ae4b7
Dimas <ndeztea@gmail.com>
2017-06-01 16:22:18 +0700

    update google analytic

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,16 +1,16 @@
 <!--script>
-       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-75151343-1', 'auto');
-      ga('send', 'pageview');
-      ga(function(tracker) {
-		  var clientId = tracker.get('clientId');
-		  jQuery('#clientId_tmp').val(clientId);
-      jQuery('#register_device_id').val(clientId);
-		});
+  ga('create', 'UA-100356134-1', 'auto');
+  ga('send', 'pageview');
 
-    </script-->
-  <input type="hidden" name="clientId_tmp" id="clientId_tmp" value="">
+  var clientId = tracker.get('clientId');
+  jQuery('#clientId_tmp').val(clientId);
+  jQuery('#register_device_id').val(clientId);
+
+</script-->
+
+<input type="hidden" name="clientId_tmp" id="clientId_tmp" value="">
