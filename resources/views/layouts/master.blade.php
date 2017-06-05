ed1848c5f44aae9e55abfe0c51b268f1f56785b3
asepkoswara <asep.koswara@gmail.com>
2017-06-05 18:32:28 +0700

    update nav

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -242,6 +242,46 @@
 
                 $('.selectpicker').select2();
             });
+
+            // Hide Header on on scroll down
+            var didScroll;
+            var lastScrollTop = 0;
+            var delta = 5;
+            var navbarHeight = $('.qm-navbar').outerHeight();
+
+            $(window).scroll(function(event){
+                didScroll = true;
+            });
+
+            setInterval(function() {
+                if (didScroll) {
+                    hasScrolled();
+                    didScroll = false;
+                }
+            }, 250);
+
+            function hasScrolled() {
+                var st = $(this).scrollTop();
+                
+                // Make sure they scroll more than delta
+                if(Math.abs(lastScrollTop - st) <= delta)
+                    return;
+                
+                // If they scrolled down and are past the navbar, add class .nav-up.
+                // This is necessary so you never see what is "behind" the navbar.
+                if (st > lastScrollTop && st > navbarHeight){
+                    // Scroll Down
+                    $('.qm-navbar').removeClass('navi-down').addClass('navi-up');
+                } else {
+                    // Scroll Up
+                    if(st + $(window).height() < $(document).height()) {
+                        $('.qm-navbar').removeClass('navi-up').addClass('navi-down');
+                    }
+                }
+                
+                lastScrollTop = st;
+            }
+
             //var vph = $(window).height();
             //$('body').css('height',vph/2).css('overflow','hidden');
             //$('.splash-body').css('height',vph);
