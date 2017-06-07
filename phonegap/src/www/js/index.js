6a9f30a86d075e4f8a7a55daf11ac1338545f4f3
Dimas <ndeztea@gmail.com>
2017-06-07 22:21:14 +0700

    frame

--- phonegap/src/www/js/index.js
+++ phonegap/src/www/js/index.js
@@ -46,7 +46,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         //app.browser = cordova.InAppBrowser.open('http://www.quranmemo.id/public/mushaf', '_blank', 'location=no,zoom=no,hidden=yes');
-        app.browser = cordova.InAppBrowser.open('http://192.168.1.2/QuranNote/public/mushaf', '_blank', 'location=no,zoom=no,hidden=yes');
+        app.browser = cordova.InAppBrowser.open('http://192.168.100.4/QuranNote/frame.html', '_blank', 'location=no,zoom=no,hidden=yes');
         // disable back
         document.addEventListener("backbutton", function (e) {
             e.preventDefault();
