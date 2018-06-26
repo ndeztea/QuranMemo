14f9339b86ef8c927f3fdbc96e39cb1e7c3df389
Dimas <dimas.santosa@happyfresh.com>
2018-06-26 14:22:30 +0700

    New Faq page

---
+++ resources/views/content_faq.blade.php
@@ -0,0 +1,40 @@
+<div  style="max-height: 523px; overflow-y: auto;padding: 10px">
+<p>
+	<strong>Sudah bayar lewat pulsa tapi belum aktif?</strong>
+	<br>
+	Konfirmasi manual terlebih dahulu, sistem kami belum bisa mengaktifasi secara otomatis
+</p>
+<p>
+	<strong>Bagaimana cara untuk konformasi?</strong>
+	<br>
+	- Ketika app terbuka pertama kali muncul popup<br>
+	atau <br>
+	- Buka Menu terus klik "Berlangganan" akan muncul popup <br>
+	Di popup itu klik "Klik disini" akan di arahkan kehalaman tagihan, dan lakukan konfirmasi jika sudah membayar atau potong pulsa, jangan lupa untuk kirimkan <strong>bukti transfer</strong>
+</p>
+<p>
+	<strong>Sudah bayar konfirmasi tapi kok lama ya aktifnya?</strong>
+	<br>
+	Tunggu 1x24 jam, atau jika ingin cepat kontak lewat WA <a href="https://api.whatsapp.com/send?phone=6285956331813&text=Assalamu'alaikum%20wr%20wb"  target="_blank" class="label label-success"><i class="fa fa-whatsapp"></i> 085956331813</a>
+</p>
+<p>
+	<strong>Aplikasi Al-Quran kok berbayar?</strong>
+	<br>
+	Hasil dari yang berlangganan akan dipergunakan untuk biaya operasional tiap bulan dan infak untuk dakwah menghafal Al-Quran
+</p>
+<p>
+	<strong>Kok tidak bisa merekam?</strong>
+	<br>
+	Pastikan menggunakan aplikasi "QuranMemo Community" jangan menggunakan aplikasi "Quranmemo", cari di playstore.
+</p>
+<p>
+	<strong>Sudah pakai aplikasi "Quranmemo Community" kok masih tidak bisa merekam?</strong>
+	<br>
+	Dibeberapa HP memang ada masalah terutama di HP vivo, karena recorder app nya beda dengan kebanyakan recorder app yang HP android kebanyakan
+</p>
+<p>
+	<strong>Apakah aplikasi QuranMemo harus terus terkoneksi ke internet?</strong>
+	<br>
+	Iya betul, kami menggunakan sistem streaming dan harus terkoneksi internet.
+</p>
+</div>
