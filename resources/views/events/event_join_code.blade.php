4a299848321e52d476373b4d0e1d749b62c108c0
Dimas <dimas.santosa@happyfresh.com>
2019-01-16 16:45:36 +0700

    Join the event file

---
+++ resources/views/events/event_join_code.blade.php
@@ -0,0 +1,5 @@
+<div class="modal-spacing">
+<p><strong>Tunjukan code ini untuk pengambilan konsumsi</strong></p><br>
+<img src="http://courtingthelaw.com/wp-content/uploads/QR-code-example.jpg" style="width:100%"/>
+<h2 style="text-align:center">1FHTT</h2>
+</div>
