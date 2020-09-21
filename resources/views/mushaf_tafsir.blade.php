05e8eea66744ceb664d7c3b92720aedf1ce5d8e4
Dimas <dimas.santosa@happyfresh.com>
2020-09-21 18:51:26 +0700

    Update layout tafsir

--- resources/views/mushaf_tafsir.blade.php
+++ resources/views/mushaf_tafsir.blade.php
@@ -1,4 +1,18 @@
 <div class="modal-spacing" style="padding: 10px">
-    <h4>{{$tafsir_header}}</h4>
-    <p>{{$tafsir}}</p>
+    <!--h4>{{$tafsir_header}}</h4-->
+
+    <div class="filter">
+    	<!-- Nav tabs -->
+    	  <ul class="nav nav-tabs" role="tablist">
+    	    <li role="presentation" class="active"><a href="#almisbah" aria-controls="profile" role="tab" data-toggle="tab">Al-Misbah</a></li>
+    	    <li role="presentation"><a href="#depag" aria-controls="profile" role="tab" data-toggle="tab">Depag</a></li>
+
+    	    <!--<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab"onclick="QuranJS.memozFilter(2)">Koreksi</a></li>-->
+    	  </ul>
+    </div>
+    <div class="tab-content">
+      <div role="tabpanel" class="tab-pane active" id="almisbah"><br><p>{{$tafsir}}</p></div>
+      <div role="tabpanel" class="tab-pane" id="depag"><br><p>{{$tafsir_depag}}</p></div>
+    </div>
+
 </div>
