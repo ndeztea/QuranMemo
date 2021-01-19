e3960fd3055befd403100a7373d402e81e90791f
Dimas <dimas.santosa@happyfresh.com>
2021-01-19 15:40:42 +0700

    Add file

---
+++ resources/views/content_voucher.blade.php
@@ -0,0 +1,10 @@
+<div class="modal-spacing">
+<p>
+Bantu kami dalam mendakwahkan dan menyebarkan aplikasi <strong>Quranmemo Community</strong> dengan cara ajak keluarga, sahabat dan teman-teman kalian untuk menggunakan aplikasi ini<br>
+<br>Jika sudah dibagikan, upload voucher ini pada saat konfirmasi berlangganan.
+</p>
+<p>
+	<img src="{{url('assets/images/voucher_islam.png')}}" style="width: 100%;"><br><br>
+	<img src="{{url('assets/images/voucher_iman.png')}}" style="width: 100%;"><br>
+</p>
+</div>
